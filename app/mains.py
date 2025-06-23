2dcb673d39fe2d4b8822b12b37bbc11a26a838a7
NIRMAL RAJ M <126080219+nirmal2971@users.noreply.github.com>
2025-06-23 18:44:01 +0530

    frontendk

--- app/mains.py
+++ app/mains.py
@@ -49,11 +49,15 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",  # for local dev
+        "https://feedback-tool-frontend.vercel.app"  # deployed frontend
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 # ✅ Include routers
 app.include_router(auth_router)
